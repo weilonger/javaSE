049b105452d447b3826b8b74076a0dc87296885c
weilonger <weilong.he@changhong.com>
2019-01-18 18:30:22 +0800

    add compete4

---
+++ src/main/java/leetcode/compete4/LongestCommonPrefix.java
@@ -0,0 +1,16 @@
+package main.java.leetcode.compete4;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        for (int i = 0; i < strs.length - 1; i++){
+            
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        LongestCommonPrefix l = new LongestCommonPrefix();
+        String[] strs = new String[]{"flower","flow","flight"};
+        System.out.println(l.longestCommonPrefix(strs));
+    }
+}
