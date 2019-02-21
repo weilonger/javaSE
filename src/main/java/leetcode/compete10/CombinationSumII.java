eb16634e48f4cf2ce08e5ed6b7e63d8ce1f1a9b2
weilonger <weilong.he@changhong.com>
2019-02-21 10:42:37 +0800

    add combinationSum

--- src/main/java/leetcode/compete10/CombinationSumII.java
+++ src/main/java/leetcode/compete10/CombinationSumII.java
@@ -5,38 +5,37 @@ import java.util.Arrays;
 import java.util.List;
 
 /*题目描述
-    39. 组合总和
-    给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
-    candidates 中的数字可以无限制重复被选取。
+    40. 组合总和 II
+    给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+    candidates 中的每个数字在每个组合中只能使用一次。
     说明：
         所有数字（包括 target）都是正整数。
         解集不能包含重复的组合。
 */
 /*示例1:
-    输入:
-        candidates = [2,3,6,7], target = 7,
+    输入: candidates = [10,1,2,7,6,1,5], target = 8,
     所求解集为:
         [
-          [7],
-          [2,2,3]
+          [1, 7],
+          [1, 2, 5],
+          [2, 6],
+          [1, 1, 6]
         ]
 */
 /*示例2:
-    输入:
-        candidates = [2,3,5], target = 8,
+    输入: candidates = [2,5,2,1,2], target = 5,
     所求解集为:
         [
-          [2,2,2,2],
-          [2,3,3],
-          [3,5]
+          [1,2,2],
+          [5]
         ]
 */
-public class CombinationSum {
-    //23ms
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+public class CombinationSumII {
+    //28ms
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> lists = new ArrayList<>();
-        Arrays.sort(candidates);
         List<Integer> list = new ArrayList<>();
+        Arrays.sort(candidates);
         int num = 0;
         find(lists, list, candidates, target, num);
         return lists;
@@ -45,24 +44,26 @@ public class CombinationSum {
     private void find(List<List<Integer>> lists, List<Integer> tmp, int[] candidates, int target, int num) {
         int len = candidates.length;
         if (target == 0) {
-            lists.add(tmp);
+            if (!lists.contains(tmp)) {
+                lists.add(tmp);
+            }
             return;
         }
-        if (target < candidates[0]) {
+        if (target < candidates[num]) {
             return;
         }
         for (int i = num; i < len && candidates[i] <= target; i++) {
             List<Integer> list = new ArrayList<>(tmp);
             list.add(candidates[i]);
-            find(lists, list, candidates, target - candidates[i], i);
+            find(lists, list, candidates, target - candidates[i], i + 1);
         }
     }
 
     public static void main(String[] args) {
-        CombinationSum c = new CombinationSum();
-        int[] candidates = new int[]{2, 3, 6 ,7};
-        int target = 7;
-        List<List<Integer>> lists = c.combinationSum(candidates, target);
+        CombinationSumII c = new CombinationSumII();
+        int[] candidates = new int[]{10, 1, 2, 7, 6, 1, 5};
+        int target = 8;
+        List<List<Integer>> lists = c.combinationSum2(candidates, target);
         for (List<Integer> list : lists) {
             list.forEach(System.out::println);
             System.out.println("-------------");
